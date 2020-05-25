bd7375ec9e28d07531b4ec0948c4c9f420a1d8dc
Grigorijs Skulteckis <grigorijs.sk@gmail.com>
2020-05-25 12:30:11 +0300

    #3 Added new validation rules for product

--- src/shoppingList/services/validations/ProductDiscountValidation.java
+++ src/shoppingList/services/validations/ProductDiscountValidation.java
@@ -13,6 +13,10 @@ public class ProductDiscountValidation implements ValidationRule<Product> {
                || product.getProductDiscount().compareTo(BigDecimal.valueOf(100.00)) > 0){
             throw new ProductValidationException("Product discount value must be in the range from 0 to 100;");
         }
+        if (product.getProductRegularPrice().compareTo(BigDecimal.valueOf(20.00)) < 0
+                && product.getProductDiscount().compareTo(BigDecimal.valueOf(0.00)) != 0){
+            throw new ProductValidationException("A discount can be set for products with a price greater than 20.00 ");
+        }
     }
 
 }
