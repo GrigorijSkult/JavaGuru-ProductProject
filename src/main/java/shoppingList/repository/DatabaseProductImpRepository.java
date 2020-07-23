9f583554f5afba055ae82c5ddcc401857dcf6a5f
User02 <grigorijs.sk@gmail.com>
2020-07-23 21:45:09 +0300

    #8 DB_SQL - JDBS temp. base - 1/7 methods

--- src/main/java/shoppingList/repository/DatabaseProductImpRepository.java
+++ src/main/java/shoppingList/repository/DatabaseProductImpRepository.java
@@ -1,19 +1,51 @@
 package shoppingList.repository;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import shoppingList.domain.ProductEntity;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Profile("mysql")
 public class DatabaseProductImpRepository implements ProductRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public DatabaseProductImpRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public ProductEntity addProduct(ProductEntity newProduct) {
-        return null;
+        String query = "INSERT INTO products (name, regular_price, category, discount, description)" +
+                "VALUES (?, ?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, newProduct.getProductName());
+            preparedStatement.setBigDecimal(2, newProduct.getProductRegularPrice());
+            preparedStatement.setString(3, String.valueOf(newProduct.getProductCategory()));
+            preparedStatement.setBigDecimal(4, newProduct.getProductDiscount());
+            preparedStatement.setString(5, newProduct.getProductDescription());
+            return preparedStatement;
+        }, keyHolder);
+        return new ProductEntity(
+                keyHolder.getKey().longValue(),
+                newProduct.getProductName(),
+                newProduct.getProductRegularPrice(),
+                newProduct.getProductCategory(),
+                newProduct.getProductDiscount(),
+                newProduct.getProductDescription()
+        );
     }
 
     @Override
@@ -28,6 +60,7 @@ public class DatabaseProductImpRepository implements ProductRepository {
 
     @Override
     public Optional<ProductEntity> findProductByID(Long id) {
+//        String query = "";
         return Optional.empty();
     }
 
@@ -38,7 +71,16 @@ public class DatabaseProductImpRepository implements ProductRepository {
 
     @Override
     public boolean existsByName(ProductEntity productEntity) {
-        return false;
+        //query problem (SQL Injection)
+        String query = "SELECT * FROM products WHERE name=" + "VALUES (?)";
+        jdbcTemplate.query(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            //////////////////////
+            preparedStatement.setString(1, productEntity.getProductName());
+            return preparedStatement;
+        });
+        List<ProductEntity> productEntities = jdbcTemplate.query(query, new BeanPropertyRowMapper<ProductEntity>());
+        return !productEntities.isEmpty();
     }
 
     @Override
