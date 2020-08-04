a4d626b5a458c6b29b7ec103394528583d049903
User02 <grigorijs.sk@gmail.com>
2020-08-04 21:27:43 +0300

    #11 ManyToMany

--- src/main/resources/DataBaseShoppingCarts.sql
+++ src/main/resources/DataBaseShoppingCarts.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA product_repository;
 
 USE product_repository;
 
+-- OneToMany and ManyToOne
+
 CREATE TABLE products
 (
     id           BIGINT      NOT NULL AUTO_INCREMENT,
@@ -24,3 +26,35 @@ CREATE TABLE shopping_carts
 
     PRIMARY KEY (id)
 );
+
+-- ManyToMany
+CREATE TABLE products
+(
+    id            BIGINT      NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(32) NOT NULL,
+    regular_price DECIMAL     NOT NULL,
+    category      VARCHAR(25) NOT NULL,
+    discount      DECIMAL,
+    description   VARCHAR(99),
+
+    PRIMARY KEY (id),
+    UNIQUE (name)
+);
+
+CREATE TABLE shopping_carts
+(
+    id          BIGINT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(50),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE products_in_shopping_carts
+(
+    shopping_cart_id BIGINT,
+    products_id      BIGINT,
+
+    FOREIGN KEY (shopping_cart_id) REFERENCES shopping_carts (id),
+    FOREIGN KEY (products_id) REFERENCES products (id)
+);
+
